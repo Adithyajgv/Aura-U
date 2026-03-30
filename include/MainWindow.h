965678433046c1e8ccf9281d49608f810bf4453b
jyothishgopa <jyothishgopa@wisc.edu>
2026-03-30 15:24:49 -0500

    moved aura page from mainWindow to Aurapage

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <adwaita.h>
 #include "DBusClient.h"
+#include "AuraPage.h"
+#include <memory>
 
 class MainWindow {
 public:
@@ -8,31 +10,9 @@ public:
 
 private:
     void buildWindow(AdwApplication* app);
-    GtkWidget* buildAuraPage();
 
-    void updateControlVisibility();
-    void applyCurrentSettings();
+    DBusClient&            m_client;
+    AdwApplicationWindow*  m_window   = nullptr;
+    std::unique_ptr<AuraPage> m_auraPage = std::make_unique<AuraPage>(m_client);
 
-    static void onModeChanged(GtkDropDown* dd, GParamSpec*, gpointer self);
-    static void onSpeedChanged(GtkRange* range, gpointer self);
-    static void onBrightnessChanged(GtkRange* range, gpointer self);
-    static void onLightbarModeChanged(GtkDropDown* dd, GParamSpec*, gpointer self);
-    static void onZoneColorSet(GtkColorDialogButton* btn, GParamSpec*, gpointer self);
-    static void onBreatheColorSet(GtkColorDialogButton* btn, GParamSpec*, gpointer self);
-
-    void syncMode(uint8_t mode);
-    void syncBrightness(uint8_t level);
-
-    DBusClient& m_client;
-
-    AdwApplicationWindow* m_window          = nullptr;
-    GtkDropDown*          m_modeDD          = nullptr;
-    GtkScale*             m_speedScale      = nullptr;
-    GtkScale*             m_brightnessScale = nullptr;
-    GtkColorDialogButton* m_zoneButtons[4]  = {};
-    GtkColorDialogButton* m_breatheColor    = nullptr;
-    GtkWidget*            m_speedRow        = nullptr;
-    GtkWidget*            m_zoneColorGroup  = nullptr;
-    GtkWidget*            m_breatheColorRow = nullptr;
-    GtkDropDown*          m_lightbarDD      = nullptr;
 };
